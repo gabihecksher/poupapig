f06043a985ba80c218fe3c4633569cddec7c6690
Gabriela <gabrielahecksher@id.uff.br>
2019-06-17 11:01:07 -0300

    criando modelos Expense e Categorie

---
+++ poupapig/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PoupapigConfig(AppConfig):
+    name = 'poupapig'
---
+++ poupapig/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Categorie(models.Model):
+	name = models.CharField(max_length=200)
+	def __str__(self):
+		return self.name
+
+class Expense(models.Model):
+	user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+	categorie = models.ForeignKey('Categorie', on_delete=models.CASCADE, blank=True, null=True, default=None)
+	date = models.DateTimeField(blank=True, null=True)
+	amount = models.FloatField(null=True, blank=True, default=None)
+	description = models.TextField()
+
+	def __str__(self):
+		return self.description
