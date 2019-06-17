6444a007c615bc180c783901296a30209925579d
Gabriela <gabrielahecksher@id.uff.br>
2019-06-17 15:03:49 -0300

    criando pagina indice

--- poupapig/urls.py
+++ poupapig/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.expense_list, name='expense_list'),
+	path('list', views.expense_list, name='expense_list'),
+	path('index', views.index, name='index'),
 ]
--- poupapig/views.py
+++ poupapig/views.py
@@ -4,3 +4,6 @@ from .models import Expense, Categorie
 def expense_list(request):
 	expenses = Expense.objects.all()
 	return render(request, 'poupapig/expense_list.html', {'expenses': expenses})
+
+def index(request):
+	return render(request, 'poupapig/index.html', {})
