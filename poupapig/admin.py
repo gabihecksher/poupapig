0a22d19ea70b91773e11b1d678fc2247f00fa9f3
Gabriela <gabrielahecksher@id.uff.br>
2019-06-17 11:46:32 -0300

    criando view para listar despesas

--- poupapig/admin.py
+++ poupapig/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Expense
+from .models import Categorie
 
 admin.site.register(Expense)
+admin.site.register(Categorie)
 # Register your models here.
--- poupapig/views.py
+++ poupapig/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def expense_list(request):
+	return render(request, 'poupapig/expense_list.html', {})
